fdbc00c74257cb7cd0edca836aeea0a0d7b34b67
EC2 Default User <ec2-user@ip-172-31-13-43.us-west-2.compute.internal>
2023-02-17 15:43:10 +0000

    added pipeline and microservice

---
+++ service-templates/ecs-fargate-service/v1/pipeline_infrastructure/lib/pipeline_infrastructure-stack.ts
@@ -0,0 +1,98 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
+import * as codepipeline_actions from 'aws-cdk-lib/aws-codepipeline-actions';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+import input from "../proton-inputs.json";
+
+export class PipelineInfrastructureStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: cdk.StackProps) {
+    super(scope, id, props);
+    
+    const aws_account_id = props?.env?.account
+    const service = input.service;
+    const service_instances = input.service_instances;
+    const pipeline_inputs = input.pipeline.inputs
+    
+    const ecr_repo = new ecr.Repository(this, "myservice_repo", {
+      repositoryName: "myservice_repo"
+    });
+    
+    const artifactsBucket = new s3.Bucket(this, "ArtifactsBucket", {
+          encryption: s3.BucketEncryption.S3_MANAGED,
+      });
+      
+    // Pipeline creation starts
+    const pipeline = new codepipeline.Pipeline(this, 'Pipeline', {
+      pipelineName: 'MultiTenantPipeline',
+      artifactBucket: artifactsBucket
+    });
+     
+    
+    // Declare source code as an artifact
+    const sourceOutput = new codepipeline.Artifact();
+
+    // Add source stage to pipeline
+    pipeline.addStage({
+      stageName: 'Source',
+      actions: [
+        new codepipeline_actions.CodeStarConnectionsSourceAction({
+          actionName: 'CodeStarSource',
+          owner: service.repository_id.split("/")[0],
+          repo: service.repository_id.split("/")[1],
+          output: sourceOutput,
+          connectionArn: service.repository_connection_arn,
+          branch: service.branch_name
+        }),
+      ],
+    });
+    
+    
+    //Build stage
+    const buildOutput = new codepipeline.Artifact();
+    
+    //Declare a new CodeBuild project
+    const buildProject = new codebuild.PipelineProject(this, 'Build', {
+      buildSpec : codebuild.BuildSpec.fromSourceFilename("your-business-microservice/buildspec.yml"),
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.AMAZON_LINUX_2_3,
+        privileged: true
+        
+      },
+      environmentVariables: {
+        'PACKAGE_BUCKET': {
+          value: artifactsBucket.bucketName
+        },
+        'aws_account_id': {
+          value: aws_account_id
+        },
+        'repo_name': {
+          value: 'myservice_repo'
+        },
+        'service_name': {
+          value: service.name
+        },
+        'dockerfile': {
+          value: pipeline_inputs.dockerfile
+        }
+        
+      }
+    });
+    
+    // Add the build stage to our pipeline
+    pipeline.addStage({
+      stageName: 'Build',
+      actions: [
+        new codepipeline_actions.CodeBuildAction({
+          actionName: 'Build-Service',
+          project: buildProject,
+          input: sourceOutput,
+          outputs: [buildOutput],
+        }),
+      ],
+    });
+    
+  }
+}
